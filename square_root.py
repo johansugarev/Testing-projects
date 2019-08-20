6fbde579ec6a660ab9fd7a6f8099f2e39f6ccbee
Johan Sugarev <johansugarev@me.com>
2019-08-20 10:58:14 +0300

    Create square_root.py

---
+++ square_root.py
@@ -0,0 +1,12 @@
+def find_square_root(x):
+    if type(x) == str:
+        return "Expected an integer! Cannot find square root of an string!"
+    for i in range(x):
+        if i ** 2 == x:
+            return i 
+    return "{} is not a perfect square".format(x)
+    
+# Test 
+x = 2
+result = find_square_root(x)
+print("Square root of {} is {}".format(x, result))
